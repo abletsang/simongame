db5bb5bbb222811a34a2cfc72602963135fce737
Able Tsang <abletsang@gmail.com>
2017-05-24 13:12:54 -0700

    fix the flashing so that if the same button flashes two or more times in a row, it lights down before lighting up again
    start function to store player moves

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var playBtn = document.querySelector(".start");
 var strictBtn = document.querySelector(".strict");
 var scoreDisplay = document.querySelector(".score");
 var order = [];
+var playerOrder = [];
 var score = 0;
 var play = false;
 var strict = false;
@@ -15,7 +16,7 @@ var turn = false;
 
 playBtn.addEventListener("click", function() {
 	if (play === false) {
-
+		playNormal();
 	} else {
 		play = false;
 		turn = false;
@@ -35,7 +36,7 @@ function playNormal() {
 	timeDown = 1000;
 	for (var i = 0; i < order.length; i ++) {
 		doLightUp(buttons[order[i]], timeUp);
-		doLightOut(buttons[order[i]], timeDown);
+		doLightOut(buttons[order[i]], timeDown, i);
 		timeUp += 1000;
 		timeDown += 1000;
 	}
@@ -43,20 +44,38 @@ function playNormal() {
 
 function doLightUp(button, time) {
   setTimeout(function() {
-  	button.classList.add("lightUp");
+  	setTimeout(function() {
+  		button.classList.add("lightUp");
+  	}, 300);
   }, time);
 }
 
-function doLightOut(button, time) {
+function doLightOut(button, time, num) {
   setTimeout(function() {
   	button.classList.remove("lightUp");
+  	if (num === order.length - 1) {
+  		turn = true;
+  		playerOrder = [];
+  	}
   }, time);
 }
 
 for (var i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", function() {
+	var i = 0;
 		if (turn === true) {
-
+			playerOrder.push(Number(this.id));
+			console.log(this.id);
+			if (playerOrder[i] !== order[i]) {
+				turn = false;
+				console.log("you lose");
+				return;
+			} else if (playerOrder.length == order.length) {
+				turn = false;
+				console.log("all right!");
+				playNormal();
+			}
+			i++;
 		}
 	});
 }
